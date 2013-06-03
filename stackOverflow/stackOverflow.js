fed47f181220cd19cec9bb19cf49cda0ba74155a
Hack Reactor Students <github@hackreactor.com>
2013-06-02 17:25:51 -0700

    Questions, answers, up and down votes

--- stackOverflow/stackOverflow.js
+++ stackOverflow/stackOverflow.js
@@ -1,5 +1,6 @@
 Users = new Meteor.Collection("users");
 Questions = new Meteor.Collection("questions");
+Answers = new Meteor.Collection("answers");
 
 if (Meteor.isClient) {
 
@@ -8,13 +9,17 @@ if (Meteor.isClient) {
     return user && user.name;
   };
   Template.dashboard.questions = function(){
-    return Questions.find({});
+    return Questions.find({}, {sort :{ created_at: -1}});
+  };
+
+  Template.question.answers = function() {
+    return Answers.find({question_id: this._id});
   };
 
   Template.questionForm.events({
     'click input.submitQuestion': function() {
       var question = document.getElementById("questionForm").value;
-      Questions.insert({user_id: Session.get("userid"), content: question});
+      Questions.insert({user_id: Session.get("userid"), content: question, created_at: new Date()});
     }
   });
 
@@ -23,6 +28,23 @@ if (Meteor.isClient) {
       var new_user = document.getElementById("enterName").value;
       var id = Users.insert({name: new_user });
       Session.set("userid",id);
+    }
+  });
+  Template.question.events({
+    'click input.submitAnswer': function(event,tmpl){
+       var response = tmpl.find('.respond').value;
+       Answers.insert({content: response, question_id: this._id, votes: 0});
+     },
+    'click input.removeQuestion': function() {
+      Questions.remove(this._id);
+    }
+  });
+  Template.answer.events({
+    'click input.upvote': function(){
+      Answers.update(this._id,{$inc: {votes: 1}});
+    },
+    'click input.downvote': function(){
+      Answers.update(this._id,{$inc: {votes: -1}});
     }
   });
 }
